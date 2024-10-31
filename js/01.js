524fcf51ffcf8059346ec6626b1599e3e5cd5426
WillMike438 <112026397+WillMike438@users.noreply.github.com>
2024-10-31 13:24:38 -0600

    Add files via upload

---
+++ js/01.js
@@ -0,0 +1,8 @@
+//variables
+var producto = "iPhone";
+var disponible;
+
+var producto1 = "Monitor",
+    producto2 = "laptop";
+
+    console.log(producto);
---
+++ js/04.js
@@ -0,0 +1,8 @@
+//Números
+for (let i = 0; i < 10; i++) {
+    if (i % 2 === 0) {
+    console.log(`El numero [${i}] es PAR`);
+    }else{
+        console.log(`El numero [${i}] es IMPAR`);
+    }
+}
